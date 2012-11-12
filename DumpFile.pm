2a1f943cb5a297a76c16cc4b1f882520659f7e47
perlino <ivanchenko.michael.work@gmail.com>
2012-11-12 18:26:56 +0200

    docs updated

--- DumpFile.pm
+++ DumpFile.pm
@@ -214,6 +214,16 @@ Read L<performance section|http://search.cpan.org/~triddle/XML-TreePuller-0.1.2/
 
 L<OpenCorpora.org|http://opencorpora.org> team.
 
+=head1 BUGS
+
+Please, report bugs to:
+
+=pod
+
+C<< print grep$_=pack((chr hex oct 77),hex),unpack join(undef,((chr hex oct 51).(chr hex oct 40))x oct hex 29),unpack chr(hex oct 104).chr hex((chr hex oct 40).(chr hex oct 75)),pack chr(hex oct 52).chr hex((chr hex oct 40).(chr hex oct 75)),unpack chr(hex oct 104).chr hex((chr hex oct 40).(chr hex oct 75)),pack chr(hex oct 60).chr hex((chr hex oct 40).(chr hex oct 75)),'011010011001101100101001110110010011100101001001101010011101100101111001111110011101000111101001011010010011100101001001001010011010100111001001110100011011101111111001000110110111100100001000101110011110100100101001011010011100100111010001001110011111100111101001' >>
+
+=cut
+
 =head1 LICENSE
 
 This program is free software, you can redistribute it under the same terms as Perl itself.
